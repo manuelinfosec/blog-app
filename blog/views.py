c8a4906b9c78174eb049b9637e57c60a9b39738a
Manuel InfoSec <manuelinfosec@gmail.com>
2022-02-05 01:05:19 +0100

    Add blog pages

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Post
 
 class BlogListView(ListView):
     model = Post
     template_name = "index.html"
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = "post_detail.html"
